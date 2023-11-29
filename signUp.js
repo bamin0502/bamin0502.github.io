ff1d6ef49bde14d2dd0a7dbd69cc856ea6e74e00
bamin0502 <100828741+bamin0502@users.noreply.github.com>
2023-11-29 22:35:06 +0900

    회원가입 최종 업데이트
    문제점 수정
    이메일, 전화번호, 아이디 등에 자동완성뜨던거 안뜨도록 수정
    비밀번호 조건을 만족하기전까지는 비밀번호 확인란 비활성화
    비밀번호랑 비밀번호 확인 서로 비교하는 비교문 추가

--- signUp.js
+++ signUp.js
@@ -11,30 +11,20 @@ const email = document.querySelector('#email');
 const mobile = document.querySelector('#mobile');
 const error = document.querySelectorAll('.error_next_box');
 
-
+window.onload = function() {
+    pw2.setAttribute("disabled", "true");
+}
 /*이벤트 핸들러 연결*/
-let isPasswordConfirmed=false;
-
 if (id) {
     id.addEventListener("focusout", checkId);
 }
 if (pw1) {
     pw1.addEventListener("focusout", checkPw);
-    pw1.addEventListener("input",function(){
-        checkPw();
-        //비밀번호 상태에 따라서 비밀번호 확인 상태를 초기화 시킴
-        isPasswordConfirmed=false;
-        togglePasswordConfirmation();
-    })
+    pw1.addEventListener("input", checkPw);
 }
 if (pw2) {
     pw2.addEventListener("focusout", comparePw);
-    pw2.addEventListener("input",function(){
-        comparePw();
-        //비밀번호 확인이 변경될때 마다 비밀번호 확인 상태를 업데이트
-        isPasswordConfirmed=pw2.value.trim()===pw1.value.trim();
-        togglePasswordConfirmation();
-    })
+    pw2.addEventListener("input", comparePw);
     pw2.addEventListener("click", function() {
         if (pw2.hasAttribute("disabled")) {
             error[2].innerHTML = "먼저 비밀번호를 입력해주세요.";
@@ -54,12 +44,12 @@ mobile.addEventListener("focusout", checkPhoneNum);
 
 
 function checkId() {
-    const idPattern = /[a-zA-Z0-9_-]{5,20}/;
+    const idPattern = /[a-zA-Z0-9]{5,20}/;
     if(id.value === "") {
         error[0].innerHTML = "필수 정보입니다.";
         error[0].style.display = "block";
     } else if(!idPattern.test(id.value)) {
-        error[0].innerHTML = "5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.";
+        error[0].innerHTML = "5~20자의 영문 소문자, 숫자만 사용 가능합니다.";
         error[0].style.display = "block";
     } else {
         error[0].innerHTML = "멋진 아이디네요!";
@@ -78,7 +68,7 @@ function checkPw() {
         togglePasswordConfirmation(false);
     } else if(!pwPattern.test(pw1.value)) {
         error[1].innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
-        pwMsg.innerHTML = "사용불가";
+        pwMsg.innerHTML = "사용 불가";
         pwMsgArea.style.paddingRight = "93px";
         error[1].style.display = "block";
         pwMsg.style.display = "block";
@@ -89,7 +79,6 @@ function checkPw() {
         error[1].style.display = "none";
         pwMsg.innerHTML = "안전";
         pwMsg.style.display = "block";
-        // 변경된 부분: 비밀번호 길이에 따라 색상 변경
         if (pw1.value.length <= 7) {
             pwMsg.style.color = "Red";
             pwImg1.src = "image/unsafe.png";
@@ -103,26 +92,24 @@ function checkPw() {
 
 function comparePw() {
      // 수정된 부분: 중복 선언 제거
-    const pw2Value = pw2.value.trim();
-
-    if (pw2Value === pw1.value && pw2Value !== "") {
-        pwImg2.src = "image/safe.png";
-        error[2].style.display = "none";
-    } else if (pw2Value !== pw1.value && pw2Value !== "") {
-        pwImg2.src = "image/unsafe.png";
-        error[2].innerHTML = "비밀번호가 일치하지 않습니다.";
-        error[2].style.display = "block";
-    } else {
-        // 비밀번호가 비어있을 때 초기화
-        pwImg2.src = "image/icon_check_disable.png";
-        error[2].innerHTML = "";
-        error[2].style.display = "none";
-    }
-
-    if (pw2Value === "") {
-        error[2].innerHTML = "필수 정보입니다.";
-        error[2].style.display = "block";
-    }
+     const pw2Value = pw2.value.trim();
+     if (pw2Value === pw1.value && pw2Value !== "") {
+         pwImg2.src = "image/safe.png";
+         error[2].style.display = "none";
+     } else if (pw2Value !== pw1.value && pw2Value !== "") {
+         pwImg2.src = "image/unsafe.png";
+         error[2].innerHTML = "비밀번호가 일치하지 않습니다.";
+         error[2].style.display = "block";
+     } else {
+         // 비밀번호가 비어있을 때 초기화
+         pwImg2.src = "image/icon_check_disable.png";
+         error[2].innerHTML = "";
+         error[2].style.display = "none";
+     }
+     if (pw2Value === "") {
+         error[2].innerHTML = "비밀번호를 먼저 입력해주세요.";
+         error[2].style.display = "block";
+     }
 }
 function togglePasswordConfirmation(isEnabled){
      // 비밀번호 확인 필드 활성화 여부 체크
